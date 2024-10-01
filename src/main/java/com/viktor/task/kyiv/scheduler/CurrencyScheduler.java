fcad603457726e2cd651069a8a54a201e47f2f1a
vshumick <vshumick@gmail.com>
2024-10-01 17:16:11 +0300

    All in one row

--- src/main/java/com/viktor/task/kyiv/scheduler/CurrencyScheduler.java
+++ src/main/java/com/viktor/task/kyiv/scheduler/CurrencyScheduler.java
@@ -2,6 +2,7 @@ package com.viktor.task.kyiv.scheduler;
 
 import com.viktor.task.kyiv.service.CurrencyService;
 import com.viktor.task.kyiv.service.ExchangeRateService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -11,13 +12,16 @@ public class CurrencyScheduler {
     private final CurrencyService currencyService;
     private final ExchangeRateService exchangeRateService;
 
+    @Value("${scheduling.fixed-rate}")
+    private long fixedRate;
+
     public CurrencyScheduler(CurrencyService currencyService, ExchangeRateService exchangeRateService) {
         this.currencyService = currencyService;
         this.exchangeRateService = exchangeRateService;
     }
 
-    // Runs every hour to update currencies and exchange rates
-    @Scheduled(fixedRate = 3600000)
+    // Runs according to the fixedRate from configuration
+    @Scheduled(fixedRateString = "${scheduling.fixed-rate}")
     public void updateCurrencyAndExchangeRates() {
         //currencyService.updateCurrencies();
         exchangeRateService.updateExchangeRates();
